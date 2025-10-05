307afcaa5386c673f3f19fcd36260a20ff0f86a9
Missile2006 <nastiamarchenko06@gmail.com>
2025-10-05 23:42:46 +0300

    Update NetSdrClient.cs
    Make '_udpClient_MessageReceived' a static method - змінено на static оскільки метод не використовує жодні поля екземпляра класу. MsgTypes _ , ControlItemCodes _ та  ushort _ більш явно показує типи параметрів. "recieved" → "received" (орфографічна помилка). Також замінено неефективне форматування байтів на BitConverter.ToString(body).Replace("-", " ")

--- NetSdrClientApp/NetSdrClient.cs
+++ NetSdrClientApp/NetSdrClient.cs
@@ -115,13 +115,13 @@ namespace NetSdrClientApp
             await SendTcpRequest(msg);
         }
 
-        private void _udpClient_MessageReceived(object? sender, byte[] e)
+        private static void _udpClient_MessageReceived(object? sender, byte[] e)
         {
-            NetSdrMessageHelper.TranslateMessage(e, out MsgTypes type, out ControlItemCodes code, out ushort sequenceNum, out byte[] body);
+            NetSdrMessageHelper.TranslateMessage(e, out MsgTypes _, out ControlItemCodes _, out ushort _, out byte[] body);
             var samples = NetSdrMessageHelper.GetSamples(16, body);
-
-            Console.WriteLine($"Samples recieved: " + body.Select(b => Convert.ToString(b, toBase: 16)).Aggregate((l, r) => $"{l} {r}"));
-
+    
+            Console.WriteLine($"Samples received: {BitConverter.ToString(body).Replace("-", " ")}");
+    
             using (FileStream fs = new FileStream("samples.bin", FileMode.Append, FileAccess.Write, FileShare.Read))
             using (BinaryWriter sw = new BinaryWriter(fs))
             {
