a1c28b5eab2971311295c0d9ffede4d74ee8aa8b
Missile2006 <nastiamarchenko06@gmail.com>
2025-10-16 19:55:14 +0300

    .

--- NetSdrClientAppTests/NetSdrClientTests.cs
+++ NetSdrClientAppTests/NetSdrClientTests.cs
@@ -1,24 +1,21 @@
 ﻿using Moq;
 using NetSdrClientApp;
 using NetSdrClientApp.Networking;
-using NetSdrClientApp.Messages;
-using System.Text;
 
 namespace NetSdrClientAppTests;
 
-[TestFixture]
 public class NetSdrClientTests
 {
-    private NetSdrClient _client = null!;
-    private Mock<ITcpClient> _tcpMock = null!;
-    private Mock<IUdpClient> _udpMock = null!;
+    NetSdrClient _client;
+    Mock<ITcpClient> _tcpMock;
+    Mock<IUdpClient> _updMock;
+
+    public NetSdrClientTests() { }
 
     [SetUp]
     public void Setup()
     {
         _tcpMock = new Mock<ITcpClient>();
-        _tcpMock.Setup(tcp => tcp.Connected).Returns(false);
-
         _tcpMock.Setup(tcp => tcp.Connect()).Callback(() =>
         {
             _tcpMock.Setup(tcp => tcp.Connected).Returns(true);
@@ -29,291 +26,94 @@ public class NetSdrClientTests
             _tcpMock.Setup(tcp => tcp.Connected).Returns(false);
         });
 
-        _tcpMock.Setup(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()))
-                .Returns(Task.CompletedTask)
-                .Callback<byte[]>(bytes =>
-                {
-                    var response = SimulateDeviceResponse(bytes);
-                    _tcpMock.Raise(tcp => tcp.MessageReceived += null, _tcpMock.Object, response);
-                });
+        _tcpMock.Setup(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>())).Callback<byte[]>((bytes) =>
+        {
+            _tcpMock.Raise(tcp => tcp.MessageReceived += null, _tcpMock.Object, bytes);
+        });
 
-        _udpMock = new Mock<IUdpClient>();
-        _udpMock.Setup(udp => udp.StartListeningAsync()).Returns(Task.CompletedTask);
-        _udpMock.Setup(udp => udp.StopListening()).Verifiable();
+        _updMock = new Mock<IUdpClient>();
 
-        _client = new NetSdrClient(_tcpMock.Object, _udpMock.Object);
+        _client = new NetSdrClient(_tcpMock.Object, _updMock.Object);
     }
 
     [Test]
-    public async Task ConnectAsync_WhenNotConnected_ConnectsAndSendsSetupMessages()
+    public async Task ConnectAsyncTest()
     {
-        // Act
+        //act
         await _client.ConnectAsync();
 
-        // Assert
+        //assert
         _tcpMock.Verify(tcp => tcp.Connect(), Times.Once);
         _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Exactly(3));
     }
 
     [Test]
-    public async Task ConnectAsync_WhenAlreadyConnected_DoesNotConnectAgain()
+    public async Task DisconnectWithNoConnectionTest()
     {
-        // Arrange
-        _tcpMock.Setup(tcp => tcp.Connected).Returns(true);
-
-        // Act
-        await _client.ConnectAsync();
-
-        // Assert
-        _tcpMock.Verify(tcp => tcp.Connect(), Times.Never);
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Never);
-    }
-
-    [Test]
-    public void Disconnect_WhenCalled_DisconnectsTcpClient()
-    {
-        // Act
+        //act
         _client.Disconnect();
 
-        // Assert
+        //assert
+        //No exception thrown
         _tcpMock.Verify(tcp => tcp.Disconnect(), Times.Once);
     }
 
     [Test]
-    public async Task StartIQAsync_WhenNotConnected_DoesNotSendMessage()
+    public async Task DisconnectTest()
     {
-        // Act
-        await _client.StartIQAsync();
+        //Arrange 
+        await ConnectAsyncTest();
 
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Never);
-        Assert.That(_client.IQStarted, Is.False);
+        //act
+        _client.Disconnect();
+
+        //assert
+        //No exception thrown
+        _tcpMock.Verify(tcp => tcp.Disconnect(), Times.Once);
     }
 
     [Test]
-    public async Task StartIQAsync_WhenConnected_SendsStartMessageAndStartsUdp()
+    public async Task StartIQNoConnectionTest()
     {
-        // Arrange
-        await _client.ConnectAsync();
 
-        // Act
+        //act
         await _client.StartIQAsync();
 
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Exactly(4)); 
-        _udpMock.Verify(udp => udp.StartListeningAsync(), Times.Once);
+        //assert
+        //No exception thrown
+        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Never);
+        _tcpMock.VerifyGet(tcp => tcp.Connected, Times.AtLeastOnce);
+    }
+
+    [Test]
+    public async Task StartIQTest()
+    {
+        //Arrange 
+        await ConnectAsyncTest();
+
+        //act
+        await _client.StartIQAsync();
+
+        //assert
+        //No exception thrown
+        _updMock.Verify(udp => udp.StartListeningAsync(), Times.Once);
         Assert.That(_client.IQStarted, Is.True);
     }
 
     [Test]
-    public async Task StopIQAsync_WhenNotConnected_DoesNotSendMessage()
+    public async Task StopIQTest()
     {
-        // Act
+        //Arrange 
+        await ConnectAsyncTest();
+
+        //act
         await _client.StopIQAsync();
 
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Never);
+        //assert
+        //No exception thrown
+        _updMock.Verify(tcp => tcp.StopListening(), Times.Once);
         Assert.That(_client.IQStarted, Is.False);
     }
 
-    [Test]
-    public async Task StopIQAsync_WhenConnectedAndIQStarted_SendsStopMessageAndStopsUdp()
-    {
-        // Arrange
-        await _client.ConnectAsync();
-        await _client.StartIQAsync();
-
-        // Act
-        await _client.StopIQAsync();
-
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Exactly(5)); 
-        _udpMock.Verify(udp => udp.StopListening(), Times.Once);
-        Assert.That(_client.IQStarted, Is.False);
-    }
-
-    [Test]
-    public async Task ChangeFrequencyAsync_WhenConnected_SendsFrequencyMessage()
-    {
-        // Arrange
-        await _client.ConnectAsync();
-        long frequency = 123456789;
-        int channel = 2;
-
-        // Act
-        await _client.ChangeFrequencyAsync(frequency, channel);
-
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.Is<byte[]>(b =>
-            b.Length > 0 &&
-            b.Skip(4).First() == (byte)channel
-        )), Times.Once);
-    }
-
-    [Test]
-    public async Task ChangeFrequencyAsync_WhenNotConnected_DoesNotSendMessage()
-    {
-        // Arrange
-        long frequency = 123456789;
-        int channel = 2;
-
-        // Act
-        await _client.ChangeFrequencyAsync(frequency, channel);
-
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Never);
-    }
-
-    [Test]
-    public async Task SetGainAsync_WhenConnected_SendsGainMessage()
-    {
-        // Arrange
-        await _client.ConnectAsync();
-        byte channel = 1;
-        byte gainValue = 50;
-
-        // Act
-        await _client.SetGainAsync(channel, gainValue);
-
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Exactly(4));
-    }
-
-    [Test]
-    public async Task SetBandwidthAsync_WhenConnected_SendsBandwidthMessage()
-    {
-        // Arrange
-        await _client.ConnectAsync();
-        byte channel = 1;
-        int bandwidth = 1000000;
-
-        // Act
-        await _client.SetBandwidthAsync(channel, bandwidth);
-
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Exactly(4));
-    }
-
-    [Test]
-    public async Task RequestDeviceStatusAsync_WhenConnected_SendsStatusRequest()
-    {
-        // Arrange
-        await _client.ConnectAsync();
-
-        // Act
-        await _client.RequestDeviceStatusAsync();
-
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Exactly(4));
-    }
-
-    [Test]
-    public async Task CalibrateDeviceAsync_WhenConnected_SendsCalibrationMessage()
-    {
-        // Arrange
-        await _client.ConnectAsync();
-
-        // Act
-        await _client.CalibrateDeviceAsync();
-
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Exactly(4));
-    }
-
-    [Test]
-    public async Task ResetDeviceAsync_WhenConnected_SendsResetMessage()
-    {
-        // Arrange
-        await _client.ConnectAsync();
-
-        // Act
-        await _client.ResetDeviceAsync();
-
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Exactly(4));
-    }
-
-    [Test]
-    public void UdpClientMessageReceived_WhenDataReceived_WritesSamplesToFile()
-    {
-        // Arrange
-        byte[] testData = CreateTestUdpData();
-        string testFileName = "samples.bin";
-
-        if (File.Exists(testFileName))
-            File.Delete(testFileName);
-
-        // Act
-        _udpMock.Raise(udp => udp.MessageReceived += null, _udpMock.Object, testData);
-
-        // Assert
-        Assert.That(File.Exists(testFileName), Is.True);
-        var fileInfo = new FileInfo(testFileName);
-        Assert.That(fileInfo.Length, Is.GreaterThan(0));
-
-        // Cleanup
-        File.Delete(testFileName);
-    }
-
-    [Test]
-    public async Task SendTcpRequest_WhenConnected_ReturnsResponse()
-    {
-        // Arrange
-        await _client.ConnectAsync();
-        byte[] testMessage = new byte[] { 0x01, 0x02, 0x03 };
-
-        await _client.RequestDeviceStatusAsync();
-
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.Exactly(4));
-    }
-
-    [Test]
-    public void TcpClientMessageReceived_WithPendingRequest_CompletesTask()
-    {
-        // Arrange
-        var testResponse = new byte[] { 0x05, 0x06, 0x07 };
-
-        Assert.DoesNotThrowAsync(async () =>
-        {
-            await _client.ConnectAsync();
-        });
-    }
-
-    [Test]
-    public async Task MultipleOperations_WhenConnected_ProcessesCorrectly()
-    {
-        // Arrange
-        await _client.ConnectAsync();
-
-        // Act - Perform multiple operations
-        await _client.StartIQAsync();
-        await _client.ChangeFrequencyAsync(100000000, 1);
-        await _client.SetGainAsync(1, 75);
-        await _client.StopIQAsync();
-
-        // Assert
-        _tcpMock.Verify(tcp => tcp.SendMessageAsync(It.IsAny<byte[]>()), Times.AtLeast(6));
-        _udpMock.Verify(udp => udp.StartListeningAsync(), Times.Once);
-        _udpMock.Verify(udp => udp.StopListening(), Times.Once);
-        Assert.That(_client.IQStarted, Is.False);
-    }
-
-    private byte[] SimulateDeviceResponse(byte[] request)
-    {
-        return new byte[] { 0x00, 0x00, 0x00, 0x01, 0x00 }; 
-    }
-
-    private byte[] CreateTestUdpData()
-    {
-        var samples = new short[] { 100, -100, 200, -200, 150, -150 };
-        using var stream = new MemoryStream();
-        using var writer = new BinaryWriter(stream);
-
-        foreach (var sample in samples)
-        {
-            writer.Write(sample);
-        }
-
-        return stream.ToArray();
-    }
+    //TODO: cover the rest of the NetSdrClient code here
 }
--- NetSdrClientAppTests/NetSdrMessageHelperTests.cs
+++ NetSdrClientAppTests/NetSdrMessageHelperTests.cs
@@ -2,7 +2,6 @@ using NetSdrClientApp.Messages;
 
 namespace NetSdrClientAppTests
 {
-    [TestFixture]
     public class NetSdrMessageHelperTests
     {
         [SetUp]
@@ -11,146 +10,60 @@ namespace NetSdrClientAppTests
         }
 
         [Test]
-        public void GetControlItemMessage_ShouldReturnValidMessage()
+        public void GetControlItemMessageTest()
         {
-            // Arrange
+            //Arrange
             var type = NetSdrMessageHelper.MsgTypes.Ack;
             var code = NetSdrMessageHelper.ControlItemCodes.ReceiverState;
-            int parametersLength = 100;
+            int parametersLength = 7500;
 
-            // Act
+            //Act
             byte[] msg = NetSdrMessageHelper.GetControlItemMessage(type, code, new byte[parametersLength]);
 
-            // Assert
-            Assert.That(msg.Length, Is.GreaterThan(4));
-            var headerBytes = msg.Take(2).ToArray();
-            var codeBytes = msg.Skip(2).Take(2).ToArray();
-            var parametersBytes = msg.Skip(4).ToArray();
+            var headerBytes = msg.Take(2);
+            var codeBytes = msg.Skip(2).Take(2);
+            var parametersBytes = msg.Skip(4);
 
-            ushort num = BitConverter.ToUInt16(headerBytes);
+            var num = BitConverter.ToUInt16(headerBytes.ToArray());
+            var actualType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
+            var actualLength = num - ((int)actualType << 13);
+            var actualCode = BitConverter.ToInt16(codeBytes.ToArray());
+
+            //Assert
+            Assert.That(headerBytes.Count(), Is.EqualTo(2));
+            Assert.That(msg.Length, Is.EqualTo(actualLength));
+            Assert.That(type, Is.EqualTo(actualType));
+
+            Assert.That(actualCode, Is.EqualTo((short)code));
+
+            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+        }
+
+        [Test]
+        public void GetDataItemMessageTest()
+        {
+            //Arrange
+            var type = NetSdrMessageHelper.MsgTypes.DataItem2;
+            int parametersLength = 7500;
+
+            //Act
+            byte[] msg = NetSdrMessageHelper.GetDataItemMessage(type, new byte[parametersLength]);
+
+            var headerBytes = msg.Take(2);
+            var parametersBytes = msg.Skip(2);
+
+            var num = BitConverter.ToUInt16(headerBytes.ToArray());
             var actualType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
             var actualLength = num - ((int)actualType << 13);
 
-            Assert.That(actualType, Is.EqualTo(type));
-            Assert.That(actualLength, Is.EqualTo(msg.Length));
-            Assert.That(BitConverter.ToUInt16(codeBytes), Is.EqualTo((ushort)code));
-            Assert.That(parametersBytes.Length, Is.EqualTo(parametersLength));
+            //Assert
+            Assert.That(headerBytes.Count(), Is.EqualTo(2));
+            Assert.That(msg.Length, Is.EqualTo(actualLength));
+            Assert.That(type, Is.EqualTo(actualType));
+
+            Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
         }
 
-        [Test]
-        public void GetDataItemMessage_ShouldReturnValidMessageWithoutCode()
-        {
-            // Arrange
-            var type = NetSdrMessageHelper.MsgTypes.DataItem1;
-            int parametersLength = 50;
-
-            // Act
-            byte[] msg = NetSdrMessageHelper.GetDataItemMessage(type, new byte[parametersLength]);
-
-            // Assert
-            Assert.That(msg.Length, Is.EqualTo(parametersLength + 2));
-            var headerBytes = msg.Take(2).ToArray();
-            ushort num = BitConverter.ToUInt16(headerBytes);
-            var actualType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
-
-            Assert.That(actualType, Is.EqualTo(type));
-        }
-
-        [Test]
-        public void TranslateMessage_ShouldExtractCorrectData_ForControlItem()
-        {
-            // Arrange
-            var type = NetSdrMessageHelper.MsgTypes.SetControlItem;
-            var code = NetSdrMessageHelper.ControlItemCodes.ManualGain;
-            var parameters = new byte[] { 0xAA, 0xBB, 0xCC };
-
-            byte[] msg = NetSdrMessageHelper.GetControlItemMessage(type, code, parameters);
-
-            // Act
-            bool success = NetSdrMessageHelper.TranslateMessage(msg, out var actualType, out var actualCode, out var seq, out var body);
-
-            // Assert
-            Assert.That(success, Is.True);
-            Assert.That(actualType, Is.EqualTo(type));
-            Assert.That(actualCode, Is.EqualTo(code));
-            Assert.That(body, Is.EqualTo(parameters));
-        }
-
-        [Test]
-        public void TranslateMessage_ShouldExtractCorrectData_ForDataItem()
-        {
-            // Arrange
-            var type = NetSdrMessageHelper.MsgTypes.DataItem2;
-            var parameters = new byte[] { 0x11, 0x22, 0x33, 0x44 };
-
-            byte[] msg = NetSdrMessageHelper.GetDataItemMessage(type, parameters);
-
-            // Act
-            bool success = NetSdrMessageHelper.TranslateMessage(msg, out var actualType, out var actualCode, out var seq, out var body);
-
-            // Assert
-            Assert.That(success, Is.True);
-            Assert.That(actualType, Is.EqualTo(type));
-            Assert.That(actualCode, Is.EqualTo(NetSdrMessageHelper.ControlItemCodes.None));
-            Assert.That(body, Is.EqualTo(parameters.Skip(2))); // 2 ·‡ÈÚË sequence
-        }
-
-        [Test]
-        public void GetSamples_ShouldReturnCorrectIntegers()
-        {
-            // Arrange
-            ushort sampleSize = 16; // 2 ·‡ÈÚË
-            byte[] body = { 0x10, 0x00, 0x20, 0x00 }; // 0x0010, 0x0020
-
-            // Act
-            var samples = NetSdrMessageHelper.GetSamples(sampleSize, body).ToList();
-
-            // Assert
-            Assert.That(samples.Count, Is.EqualTo(2));
-            Assert.That(samples[0], Is.EqualTo(0x0010));
-            Assert.That(samples[1], Is.EqualTo(0x0020));
-        }
-
-        [Test]
-        public void GetSamples_WithInvalidSampleSize_ShouldThrow()
-        {
-            // Arrange
-            ushort sampleSize = 64;
-            byte[] body = new byte[10];
-
-            // Act + Assert
-            Assert.Throws<ArgumentOutOfRangeException>(() => NetSdrMessageHelper.GetSamples(sampleSize, body).ToList());
-        }
-
-        [Test]
-        public void GetMessage_ShouldThrow_WhenLengthTooLong()
-        {
-            // Arrange
-            var type = NetSdrMessageHelper.MsgTypes.Ack;
-            var parameters = new byte[9000]; // too long
-
-            // Act + Assert
-            Assert.Throws<ArgumentException>(() =>
-                typeof(NetSdrMessageHelper)
-                .GetMethod("GetMessage", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)!
-                .Invoke(null, new object[] { type, NetSdrMessageHelper.ControlItemCodes.None, parameters })
-            );
-        }
-
-        [Test]
-        public void TranslateHeader_ShouldHandleDataItemEdgeCase()
-        {
-            // Arrange
-            var type = NetSdrMessageHelper.MsgTypes.DataItem0;
-            byte[] header = BitConverter.GetBytes((ushort)((int)type << 13));
-
-            // Act
-            typeof(NetSdrMessageHelper)
-                .GetMethod("TranslateHeader", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)!
-                .Invoke(null, new object[] { header, null!, null! });
-
-            // Just checking that it doesnít throw
-            Assert.Pass("No exception thrown on edge case.");
-        }
+        //TODO: add more NetSdrMessageHelper tests
     }
 }
